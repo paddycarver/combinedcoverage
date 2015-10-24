9b7f2c485d1bbf33c6821cca12a53a95b44df1f5
Paddy Foran <paddy@dramafever.com>
2015-10-23 20:23:55 -0700

    First commit.
    First pass at a tool that will just print out the percent of statements
    across all the passed coverage files that can be considered "covered".

---
+++ combined.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"golang.org/x/tools/cover"
+)
+
+// percentCovered returns, as a percentage, the fraction of the statements in
+// the profiles passed in.
+// In effect, it reports the coverage of a given set of source files.
+func percentCovered(p []*cover.Profile) float64 {
+	var total, covered int64
+	for _, prof := range p {
+		for _, b := range prof.Blocks {
+			total += int64(b.NumStmt)
+			if b.Count > 0 {
+				covered += int64(b.NumStmt)
+			}
+		}
+	}
+	if total == 0 {
+		return 0
+	}
+	return float64(covered) / float64(total) * 100
+}
+
+func main() {
+	flag.Parse()
+
+	var profiles []*cover.Profile
+
+	for _, file := range flag.Args() {
+		profs, err := cover.ParseProfiles(file)
+		if err != nil {
+			log.Fatalf("failed to parse profiles: %v", err)
+		}
+		profiles = append(profiles, profs...)
+	}
+	fmt.Printf("coverage: %.1f%%", percentCovered(profiles))
+}
